f3f9390fbff5e3cad57573bf3b61bbd1bada756f
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-08-12 23:18:33 -0400

    Wait until authIsReady to be true to render components

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 import Navbar from './components/Navbar'
+import { useAuthContext } from './hooks/useAuthContext'
 
 function App() {
+  const { authIsReady } = useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route path="/login" component={Login}/>
-          <Route path="/signup" component={Signup}/>
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/login" component={Login}/>
+            <Route path="/signup" component={Signup}/>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
