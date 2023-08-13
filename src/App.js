5068efb80784905bc78f987df84d63031a58ae28
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-08-12 23:33:49 -0400

    Setup Re-direct routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar'
 import { useAuthContext } from './hooks/useAuthContext'
 
 function App() {
-  const { authIsReady } = useAuthContext();
+  const { authIsReady, user } = useAuthContext();
 
   return (
     <div className="App">
@@ -14,9 +14,18 @@ function App() {
         <BrowserRouter>
           <Navbar />
           <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/signup" component={Signup}/>
+            <Route exact path="/">
+              {!user && <Redirect to="/login" />}
+              {user && <Home />}
+            </Route>
+            <Route path="/login">
+              {user && <Redirect to="/" />}
+              {!user && <Login />}
+            </Route>
+            <Route path="/signup">
+            {user && <Redirect to="/" />}
+            {!user && <Signup />}
+            </Route>
           </Switch>
         </BrowserRouter>
       )}
