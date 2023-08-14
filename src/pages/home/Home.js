57bfd336e578dd809227b294de8a2c925fb083e3
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-08-14 13:09:13 -0400

    order transactions

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -10,14 +10,17 @@ import TransactionList from './TransactionList'
 
 export default function Home() {
     const { user } = useAuthContext();
-    const { documents, error } = useCollection('transactions',
-    ['uid', '==', user.uid]);
+    const { documents, error } = useCollection(
+        'transactions',
+        ['uid', '==', user.uid],
+        [`createdAt`, 'desc']
+    );
 
     return (
         <div className={styles.container}>
         <div className={styles.content}>
-            { error && <p>{error}</p> };
-            { documents && <TransactionList transactions={documents} /> };
+            { error && <p>{error}</p> }
+            { documents && <TransactionList transactions={documents} /> }
         </div>
         <div className={styles.sidebar}>
             <TransactionForm uid={user.uid} />
