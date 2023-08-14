3d565fa0ce2290bf05f0a9fd81610905585a87bb
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-08-14 12:42:22 -0400

    Add transaction list using realtime data

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,30 +1,32 @@
-import { useState, useEffect } from 'react';
-import { projectFirestore } from '../firebase/config';
+import { useEffect, useState } from "react"
+import { projectFirestore } from "../firebase/config"
 
 export const useCollection = (collection) => {
-    const [documents, setDocuments] = useState(null);
-    const [error, setError] = useState(null);
+    const [documents, setDocuments] = useState(null)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
-        let ref = projectFirestore.collection(collection);
+        let ref = projectFirestore.collection(collection)
 
-        const unsubscribe = ref.onSnapshot((snapshot) => {
-            let results = [];
-            snapshot.docs.forEach(doc => {
-                results.push({ ...doc.data(), id: doc.id })
-            })
-
-            // update state
-            setDocuments(results);
-            setError(null);
-        }, (error) => {
-            console.log(error);
-            setError("Could not fetch the data");
+        const unsubscribe = ref.onSnapshot(snapshot => {
+        let results = []
+        snapshot.docs.forEach(doc => {
+            console.log(doc)
+            results.push({...doc.data(), id: doc.id})
+        });
+        
+        // update state
+        setDocuments(results)
+        setError(null)
+        }, error => {
+        console.log(error)
+        setError('could not fetch the data')
         })
 
         // unsubscribe on unmount
-        return () => unsubscribe();
-    }, [collection]);
+        return () => unsubscribe()
 
-    return { documents, error };
+    }, [collection])
+
+    return { documents, error }
 }
